d1a49ba089783e5748bd9192c7eb5f3769174968
Elliot Jackson <elliotekj@gmail.com>
2017-03-10 18:28:39 +0100

    Improve the glob regex
    — Elliot Jackson <elliot@elliotekj.com>

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ pub struct View {
 lazy_static! {
     pub static ref BUILD_PATH: String = env::var("BUILD_PATH").unwrap();
     pub static ref CONTENT_PATH: String = env::var("CONTENT_PATH").unwrap();
-    pub static ref GLOB_FOR_LOOP_REGEX: Regex = Regex::new(r#"(\{%\sfor\s\w*\sin)\s(")([^\s]*)(")\s(%})"#).unwrap();
+    pub static ref GLOB_FOR_LOOP_REGEX: Regex = Regex::new(r#"(\{%\sfor\s\w*\sin)\s"([^\s]*)"\s(%})"#).unwrap();
     pub static ref SITE_NAME: String = env::var("SITE_NAME").unwrap();
     pub static ref TERA: Tera = views::get_tera();
     pub static ref THEME_PATH: String = env::var("THEME_PATH").unwrap();
